5111137d2fd073469c46b5e433b009b3e2d473a8
EUBIN <41097199+danial1021@users.noreply.github.com>
2020-09-07 13:58:02 +0900

    settting koa server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const Koa = require('koa')
+const mongoose = require('mongoose')
+const app = new Koa()
+
+const mongoURI = 'mongodb://localhost:27017/Taekwondo'
+
+mongoose
+  .connect(
+    mongoURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB 연결 성공'))
+  .catch(err => console.log(err))
+
+app.use(ctx => {
+  ctx.body = 'Hello Koa'
+})
+
+app.listen(4000, () => {
+  console.log('node server is listening to port 4000')
+})
