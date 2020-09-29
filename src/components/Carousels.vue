3b9de4ec2137d31c994e9982fc45c3b39265bf88
EUBIN <41097199+danial1021@users.noreply.github.com>
2020-09-29 09:41:49 +0900

    create Carousels.vue position adjustment

---
+++ src/components/Carousels.vue
@@ -0,0 +1,65 @@
+
+<template>
+  <div class="size">
+    <v-carousel
+      cycle
+      height="40vh"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item
+        v-for="(slide, i) in slides"
+        :key="i"
+      >
+        <v-sheet
+          :color="colors[i]"
+          height="100%"
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="display-3">
+              {{ slide }} Slide
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousels',
+
+  data: () => ({
+    colors: [
+      'indigo',
+      'warning',
+      'pink darken-2',
+      'red lighten-1',
+      'deep-purple accent-4'
+    ],
+    slides: [
+      'First',
+      'Second',
+      'Third',
+      'Fourth',
+      'Fifth'
+    ]
+  })
+}
+</script>
+
+<style scoped>
+.size {
+  background-color: #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 48vh;
+  flex-direction: column;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
     <Header />
+    <Nav v-if="$store.state.drawer == true"/>
+
+    <v-main>
+      <Carousels />
+    </v-main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
+import Nav from '@/components/Nav.vue'
+import Carousels from '@/components/Carousels.vue'
 
 export default {
   name: 'Home',
 
   components: {
-    Header
+    Header,
+    Nav,
+    Carousels
   },
 
   data: () => ({
