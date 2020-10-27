fc3f957f0a0ed43fe87a13c324872528f292947f
EUBIN <41097199+danial1021@users.noreply.github.com>
2020-10-27 21:22:20 +0900

    simple login form

--- src/views/AdminLogin.vue
+++ src/views/AdminLogin.vue
@@ -3,18 +3,21 @@
     <v-main>
       <Login />
     </v-main>
+    <FixedFooter />
   </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
 import Login from '@/components/Login.vue'
+import FixedFooter from '@/components/layout/FixedFooter.vue'
 
 export default {
   name: 'AdminLogin',
 
   components: {
-    Login
+    Login,
+    FixedFooter
   },
 
   data: () => ({
